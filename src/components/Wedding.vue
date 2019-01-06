94e4fa42552498100801d8d14a95d6ab9c7860c2
杨晓晨 <18730222889@126.com>
2019-01-06 16:33:24 +0800

    some change

--- src/components/Wedding.vue
+++ src/components/Wedding.vue
@@ -2,7 +2,9 @@
   <div>
     <div class="wedding-user-info">
       <div class="wedding-user-info-left">
-        <img src alt>
+        <img
+          src="https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=347837335,1617350328&fm=27&gp=0.jpg"
+        >
       </div>
       <div class="wedding-user-info-right">
         <div class="wedding-user-info-name">单尾羊羊</div>
@@ -10,33 +12,49 @@
           <img src="../assets/location.png">
           <label>北京</label>
         </div>
-        <div class="wedding-user-info sub">
+        <div class="wedding-user-info-sub">
           <img src="../assets/history.png">
           <label>婚期：2019-7-7</label>
         </div>
       </div>
     </div>
-    <div class="wedding-cell van-hairline--bottom wedding-photo-board">
+    <div class="wedding-cell wedding-photo-board">
       <div class="wedding-common-cell-header">
         <div class="wedding-common-cell-header-title">婚礼风格愿景板</div>
       </div>
-      <div class="wedding-photo-board-content"></div>
+      <div class="wedding-photo-board-content">
+        <div>
+          <vue-gallery-pictures
+            :items="items"
+            :boxInitRatio=77
+            :box-container-class="boxContainerClass"
+            last-line-mode="origin"
+            @loaded="loaded"
+            @error="error"
+          >
+            <template slot-scope="scope">
+              <span v-show="scope.item.error">error</span>
+              <span v-show="scope.item.loading && !scope.item.error">loading</span>
+            </template>
+          </vue-gallery-pictures>
+        </div>
+      </div>
     </div>
     <div class="wedding-cell wedding-photo-board">
       <div class="wedding-common-cell-header">
-        <div>婚礼性格</div>
+        <div class="wedding-common-cell-header-title">婚礼性格</div>
       </div>
       <div class="wedding-photo-board-content"></div>
     </div>
     <div class="wedding-cell wedding-photo-board">
       <div class="wedding-common-cell-header">
-        <div>色彩风格</div>
+        <div class="wedding-common-cell-header-title">色彩风格</div>
       </div>
       <div class="wedding-photo-board-content"></div>
     </div>
     <div class="wedding-cell van-hairline--bottom wedding-photo-board">
       <div class="wedding-common-cell-header">
-        <div>必要记录</div>
+        <div class="wedding-common-cell-header-title">必要记录</div>
       </div>
       <div class="wedding-photo-board-content"></div>
     </div>
@@ -47,13 +65,88 @@
 </template>
 
 <script>
-export default {};
+import VueGalleryPictures from "vue-gallery-pictures";
+import axios from "axios";
+
+export default {
+  components: {
+    VueGalleryPictures
+  },
+  data() {
+    return {
+      items: [],
+      boxContainerClass: "container-class"
+    };
+  },
+  methods: {
+    loaded(item) {
+      // console.log(item.loading);
+    },
+    error(item, e) {
+      // console.log(item.error, e);
+    }
+  },
+  created() {
+    const basePath = "https://xieranmaya.github.io/images/cats";
+    axios.get(`${basePath}/cats.json`).then(res => {
+      res.data.forEach(item => {
+        item.src = basePath + "/" + item.url;
+        item.loading = true;
+        item.error = false;
+        item.width = parseInt(item.width / 30);
+        item.height = parseInt(item.height / 30);
+      });
+      console.log(res.data);
+      this.items = res.data;
+    });
+  }
+};
 </script>
 
 <style lang="less">
 .wedding {
   &-user-info {
     padding: 29px 40px;
+    display: flex;
+    flex-direction: row;
+    margin-bottom: 8px;
+    background: #ffffff;
+    box-shadow: 0 3px 10px 0 rgba(0, 0, 0, 0.09);
+  }
+  &-user-info-left img {
+    width: 74px;
+    height: 74px;
+    border-radius: 37px;
+    margin-top: 6px;
+  }
+  &-user-info-name {
+    font-family: PingFangSC-Medium;
+    font-size: 16px;
+    color: #000000;
+    letter-spacing: 0;
+    text-align: center;
+    line-height: 30px;
+    text-align: left;
+    margin-left: 13px;
+  }
+  &-user-info-sub {
+    display: flex;
+    flex-direction: row;
+    margin-left: 13px;
+    margin-top: 8px;
+  }
+  &-user-info-sub img {
+    width: 18px;
+    height: 18px;
+    margin-top: 1px;
+  }
+  &-user-info-sub label {
+    font-family: PingFangSC-Regular;
+    font-size: 14px;
+    color: #555555;
+    letter-spacing: 0;
+    line-height: 20px;
+    margin-left: 3px;
   }
   &-cell {
     background: #ffffff;
@@ -65,6 +158,7 @@ export default {};
     height: 51px;
     display: flex;
     flex-direction: row;
+    border-bottom: 1px solid #eeeeee;
   }
   &-common-cell-header-title {
     font-family: PingFangSC-Medium;
@@ -72,9 +166,19 @@ export default {};
     color: #000000;
     letter-spacing: 0;
     line-height: 51px;
+    margin-left: 18px;
   }
   &-photo-board-content {
-    height: 50px;
+    // height: 50px;
+    max-height: 200px;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    overflow-y: hidden;
   }
+}
+.container-class {
+  margin: 4px;
+  border-radius: 5px;
 }
 </style>
