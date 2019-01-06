ebd51b4929edcc6f41bbb0e253ea967405a6896b
杨晓晨 <18730222889@126.com>
2019-01-06 12:19:17 +0800

    主题结构完成

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <!-- <van-nav-bar
+      title="标题"
+      left-text="返回"
+      right-text="按钮"
+      left-arrow
+      @click-left="onClickLeft"
+      @click-right="onClickRight"
+    /> -->
+    <wedding />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Wedding from './components/Wedding'
+import { NavBar, Toast } from "vant";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    [NavBar.name]: NavBar,
+    Toast,
+    wedding: Wedding
+  },
+  methods: {
+    onClickLeft() {
+      Toast('暂无后续逻辑~');
+    },
+
+    onClickRight() {
+      Toast('暂无后续逻辑~');
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background-color: #f4f4f4;
 }
 </style>
---
+++ src/components/Wedding.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <div class="wedding-user-info">
+      <div class="wedding-user-info-left">
+        <img src alt>
+      </div>
+      <div class="wedding-user-info-right">
+        <div class="wedding-user-info-name">单尾羊羊</div>
+        <div class="wedding-user-info-sub">
+          <img src="../assets/location.png">
+          <label>北京</label>
+        </div>
+        <div class="wedding-user-info sub">
+          <img src="../assets/history.png">
+          <label>婚期：2019-7-7</label>
+        </div>
+      </div>
+    </div>
+    <div class="wedding-cell van-hairline--bottom wedding-photo-board">
+      <div class="wedding-common-cell-header">
+        <div class="wedding-common-cell-header-title">婚礼风格愿景板</div>
+      </div>
+      <div class="wedding-photo-board-content"></div>
+    </div>
+    <div class="wedding-cell wedding-photo-board">
+      <div class="wedding-common-cell-header">
+        <div>婚礼性格</div>
+      </div>
+      <div class="wedding-photo-board-content"></div>
+    </div>
+    <div class="wedding-cell wedding-photo-board">
+      <div class="wedding-common-cell-header">
+        <div>色彩风格</div>
+      </div>
+      <div class="wedding-photo-board-content"></div>
+    </div>
+    <div class="wedding-cell van-hairline--bottom wedding-photo-board">
+      <div class="wedding-common-cell-header">
+        <div>必要记录</div>
+      </div>
+      <div class="wedding-photo-board-content"></div>
+    </div>
+    <div>
+      <button>重新测试</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less">
+.wedding {
+  &-user-info {
+    padding: 29px 40px;
+  }
+  &-cell {
+    background: #ffffff;
+    box-shadow: 0 3px 10px 0 rgba(0, 0, 0, 0.09);
+    border-radius: 13px;
+    margin: 0 10px 8px;
+  }
+  &-common-cell-header {
+    height: 51px;
+    display: flex;
+    flex-direction: row;
+  }
+  &-common-cell-header-title {
+    font-family: PingFangSC-Medium;
+    font-size: 16px;
+    color: #000000;
+    letter-spacing: 0;
+    line-height: 51px;
+  }
+  &-photo-board-content {
+    height: 50px;
+  }
+}
+</style>
